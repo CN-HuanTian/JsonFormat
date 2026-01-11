db5001b8789280193988ce42526e2c58e543b869
HuanTian <HuanTian3365543554@outlook.com>
2026-01-11 15:40:27 +0800

    refactor:去除无用代码

--- src/main.ts
+++ src/main.ts
@@ -5,13 +5,6 @@ import 'virtual:uno.css'
 // element-plus
 import 'element-plus/dist/index.css'
 import 'element-plus/theme-chalk/dark/css-vars.css'
-// //highlight.js
-// import 'highlight.js/styles/stackoverflow-light.css'
-// import hljs from 'highlight.js'
-// import json from 'highlight.js/lib/languages/json'
-// import hljsVuePlugin from "@highlightjs/vue-plugin";
-// hljs.registerLanguage('json', json);
 
 const app = createApp(App)
-// app.use(hljsVuePlugin)
 app.mount('#app')
